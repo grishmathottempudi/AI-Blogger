9f4d7ac29e4831387546b02e9d9744077f354184
grishmathottempudi <grishma.thottempudi@gmail.com>
2026-07-17 11:23:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,41 @@
-!pip install groq
-
-import os
-from groq import Groq
+!pip -q install google-genai gradio
+from google import genai
 import gradio as gr
-
-client = Groq(api_key="GEMINI-API-KEY")
-
-def generate_blog(restaurant, food, audience, tone, words, language):
-    prompt = f"""
-Write a {words}-word blog in {language}.
-
-Restaurant: {restaurant}
-Topic/Dish: {food}
-Audience: {audience}
-Tone: {tone}
-
-Include:
-- Restaurant introduction
-- Description of the delicious food
-- A rating out of 5
-- A conclusion
-"""
-
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-    )
-
-    blog_text = response.choices[0].message.content
-
-    filename = f"{restaurant.replace(' ', '_')}_blog.txt"
-
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(blog_text)
-
-    return blog_text, filename
-
-
-demo = gr.Interface(
+client = genai.Client(api_key="GEMINI-AP-KEY")
+def generate_blog(topic, audience, tone, words):
+  prompt = f"""
+  write a {words}-word blog.
+  Topic: {topic}
+  Audience: {audience}
+  Tone: {tone}
+  Include:
+  - Introduction
+  - Fun Facts
+  - Tips
+  - Advantages
+  - Conclusion
+  """
+  response=client.models.generate_conten(
+      model="gemini-2.5-flash",
+      contents=prompt
+  )
+  return response,text
+demo=gr.Interface(
     fn=generate_blog,
     inputs=[
-        gr.Textbox(label="Restaurant Name"),
-        gr.Textbox(label="Food/Topic"),
+        gr.Textbox(label="Topic"),
         gr.Textbox(label="Audience"),
         gr.Dropdown(
-            ["Professional", "Casual", "Funny", "Technical", "Inspirational"],
+            ["Proffesional","Casual","Funny","Sarcastic","Sad","Technical"],
             label="Tone"
         ),
-        gr.Slider(
-            minimum=200,
-            maximum=1000,
-            value=500,
-            label="Word Count"
+        gr.Slider(200,1000,value=500,label="Word Count"),
+        gr.Radio(
+            ["Informative", "Storytelling", "SEO-Friendly"],
+            label="Blog Style"
         ),
-        gr.Dropdown(
-            ["English", "Telugu", "Hindi", "Tamil", "Kannada", "Spanish", "French"],
-            label="Language",
-            value="English"
-        )
     ],
-    outputs=[
-        gr.Markdown(label="Generated Blog"),
-        gr.File(label="Download Blog")
-    ],
-    title="AI Blog Generator"
+    outputs="markdown",
+    title="AI Blog Generator (Gemini)"
 )
-
-demo.launch()
+demo.launch() blog
